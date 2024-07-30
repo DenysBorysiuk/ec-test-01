89aaf45dadf690a7e76142afdc2d67d6405e382d
Denys Borysiuk <denisborisiuk@gmail.com>
2024-07-30 16:16:18 +0300

    add alt's

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import IButton from './components/IButton/IButton.vue'
   <main class="flex h-screen">
     <section class="flex-1 flex justify-center items-center px-5 bg-primary">
       <div class="text-white text-center">
-        <img class="inline mb-6" src="./assets/img/map-pin.svg" alt="" />
+        <img class="inline mb-6" src="./assets/img/map-pin.svg" alt="pin icon" />
 
         <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
 
@@ -14,12 +14,13 @@ import IButton from './components/IButton/IButton.vue'
           Простий і зручний вуб додаток, який дозволить тобі відмічати твої улюблені місця, а також
           ті, в яких би ти дуже хотів побувати. Тож не зволікай і спробуй сам.
         </p>
+
         <IButton></IButton>
       </div>
     </section>
 
     <section class="flex-1">
-      <img class="h-full w-full object-cover" src="./assets/img/static-map.png" alt="" />
+      <img class="h-full w-full object-cover" src="./assets/img/static-map.png" alt="map" />
     </section>
   </main>
 </template>
