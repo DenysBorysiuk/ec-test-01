eef480cb1d97fafadbc22665ed7206734b6ab5a6
Denys Borysiuk <denisborisiuk@gmail.com>
2024-08-01 17:45:08 +0300

    add v-model

--- src/components/RegistrationForm/RegistrationForm.vue
+++ src/components/RegistrationForm/RegistrationForm.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { reactive, toRaw } from 'vue'
 import IInput from '../IInput/IInput.vue'
 import IButton from '../IButton/IButton.vue'
 
@@ -10,15 +11,25 @@ const props = defineProps({
 })
 
 const emit = defineEmits(['submit'])
+const userData = reactive({
+  name: '',
+  email: '',
+  password: ''
+})
 </script>
 
 <template>
   <form @submit.prevent="emit('submit', toRaw(userData))">
-    <IInput class="mb-4" label="Повне ім'я" />
+    <IInput class="mb-4" label="Повне ім'я" v-model="userData.name" />
 
-    <IInput class="mb-4" label="Електронна пошта" placeholder="email@gmail.com" />
+    <IInput
+      class="mb-4"
+      label="Електронна пошта"
+      placeholder="email@gmail.com"
+      v-model="userData.email"
+    />
 
-    <IInput label="Пароль" type="password" />
+    <IInput label="Пароль" type="password" v-model="userData.password" />
 
     <IButton class="mt-10 w-full" variant="gradient" type="submit" :is-loading="props.isLoading">
       Створити аккаунт
