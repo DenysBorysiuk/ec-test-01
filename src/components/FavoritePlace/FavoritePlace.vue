4cfd9a7f43e4c9ff2def474788dcb21a08d0382c
Denys Borysiuk <denisborisiuk@gmail.com>
2024-08-02 20:24:14 +0300

    fix icons

--- src/components/FavoritePlace/FavoritePlace.vue
+++ src/components/FavoritePlace/FavoritePlace.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { defineEmits } from 'vue'
 import FavoritePlaceIconButton from './FavoritePlaceIconButton.vue'
-import DeleteIcon from './DeleteIcon.vue'
-import EditIcon from './EditIcon.vue'
+import DeleteIcon from '../icons/DeleteIcon.vue'
+import EditIcon from '../icons/EditIcon.vue'
 import fallbackImage from '../../assets/img/ukraine.png'
 
 const props = defineProps({
