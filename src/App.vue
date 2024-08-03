822684332c553182ac4a55eefa0f50a2768f4cbd
Denys Borysiuk <denisborisiuk@gmail.com>
2024-08-03 14:00:29 +0300

    add map settings

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <script setup>
 // import HomePage from './views/HomePage.vue'
-// import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
+import { MapboxMap } from '@studiometa/vue-mapbox-gl'
+import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 // import RegistrationForm from './components/RegistrationForm/RegistrationForm.vue'
-import CreateNewPlaceModal from './components/CreateNewPlaceModal/CreateNewPlaceModal.vue'
+// import CreateNewPlaceModal from './components/CreateNewPlaceModal/CreateNewPlaceModal.vue'
 </script>
 
 <template>
   <!-- <HomePage /> -->
-  <!-- <div class="bg-white h-screen w-[400px]">
-    <FavoritePlaces />
-  </div> -->
+
   <!-- <RegistrationForm /> -->
-  <CreateNewPlaceModal />
+  <!-- <CreateNewPlaceModal /> -->
+  <main class="flex h-screen">
+    <div class="bg-white h-full w-[400px] shrink-0 overflow-auto pb-10">
+      <FavoritePlaces />
+    </div>
+
+    <div class="w-full h-full flex items-center justify-center text-6xl">
+      <MapboxMap class="w-full h-full"></MapboxMap>
+    </div>
+  </main>
 </template>
