e139029b5e48b416a20e9c140b5ac608095b9f36
Denys Borysiuk <denisborisiuk@gmail.com>
2024-08-03 14:12:04 +0300

    add map to page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 // import HomePage from './views/HomePage.vue'
 import { MapboxMap } from '@studiometa/vue-mapbox-gl'
+import { mapSettings } from './map/settings'
 import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 // import RegistrationForm from './components/RegistrationForm/RegistrationForm.vue'
 // import CreateNewPlaceModal from './components/CreateNewPlaceModal/CreateNewPlaceModal.vue'
@@ -17,7 +18,13 @@ import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
     </div>
 
     <div class="w-full h-full flex items-center justify-center text-6xl">
-      <MapboxMap class="w-full h-full"></MapboxMap>
+      <MapboxMap
+        class="w-full h-full"
+        :access-token="mapSettings.apiToken"
+        :map-style="mapSettings.style"
+        :center="[30.5245, 50.4504]"
+        :zoom="10"
+      ></MapboxMap>
     </div>
   </main>
 </template>
