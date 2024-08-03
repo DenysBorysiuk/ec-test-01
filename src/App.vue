42b6fc1926759cfb15fc50c17cfd5e32860a3ee4
Denys Borysiuk <denisborisiuk@gmail.com>
2024-08-03 14:41:29 +0300

    add markers to map

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,34 @@
 <script setup>
 // import HomePage from './views/HomePage.vue'
-import { MapboxMap } from '@studiometa/vue-mapbox-gl'
+import { MapboxMap, MapboxMarker } from '@studiometa/vue-mapbox-gl'
 import { mapSettings } from './map/settings'
 import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
+import MarkerIcon from './components/icons/MarkerIcon.vue'
 // import RegistrationForm from './components/RegistrationForm/RegistrationForm.vue'
 // import CreateNewPlaceModal from './components/CreateNewPlaceModal/CreateNewPlaceModal.vue'
+
+const favoritePlaces = [
+  {
+    id: 1,
+    title: 'New place 1',
+    description: 'SUper description 1',
+    img: '',
+    lngLat: [30.5245, 50.4514]
+  },
+  {
+    id: 2,
+    title: 'New place 2',
+    description: 'SUper description 2',
+    img: '',
+    lngLat: [30.5245, 50.4504]
+  }
+]
 </script>
 
 <template>
-  <!-- <HomePage /> -->
-
-  <!-- <RegistrationForm /> -->
-  <!-- <CreateNewPlaceModal /> -->
   <main class="flex h-screen">
     <div class="bg-white h-full w-[400px] shrink-0 overflow-auto pb-10">
-      <FavoritePlaces />
+      <FavoritePlaces :items="favoritePlaces" />
     </div>
 
     <div class="w-full h-full flex items-center justify-center text-6xl">
@@ -24,7 +38,11 @@ import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
         :map-style="mapSettings.style"
         :center="[30.5245, 50.4504]"
         :zoom="10"
-      ></MapboxMap>
+      >
+        <MapboxMarker v-for="place in favoritePlaces" :key="place.id" :lngLat="place.lngLat">
+          <MarkerIcon></MarkerIcon
+        ></MapboxMarker>
+      </MapboxMap>
     </div>
   </main>
 </template>
--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,13 +1,34 @@
 <script setup>
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
+
+const props = defineProps({
+  items: {
+    required: true,
+    type: Array
+  },
+  activeId: {
+    required: true,
+    type: [String, null]
+  },
+  isPlacesLoading: {
+    type: Boolean,
+    default: false
+  }
+})
 </script>
 
 <template>
   <div class="px-6">
     <p class="text-gray mb-4">Додані маркери</p>
 
-    <FavoritePlace v-for="n in 4" :key="n" />
+    <FavoritePlace
+      v-for="place in props.items"
+      :key="place.id"
+      :title="place.title"
+      :description="place.description"
+      :img="place.img"
+    />
 
     <IButton class="w-full mt-10" variant="gradient">Додати маркер</IButton>
   </div>
