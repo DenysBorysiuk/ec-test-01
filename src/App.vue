8c58cf9d3132f748ae6b0f7d6584077bfe5dcf77
Denys Borysiuk <denisborisiuk@gmail.com>
2024-08-03 22:00:39 +0300

    refactoring App

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,7 @@
 <script setup>
-// import HomePage from './views/HomePage.vue'
-import { MapboxMap, MapboxMarker } from '@studiometa/vue-mapbox-gl'
-import { mapSettings } from './map/settings'
-import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
-import MarkerIcon from './components/icons/MarkerIcon.vue'
-import { ref } from 'vue'
-// import RegistrationForm from './components/RegistrationForm/RegistrationForm.vue'
-// import CreateNewPlaceModal from './components/CreateNewPlaceModal/CreateNewPlaceModal.vue'
-
-const favoritePlaces = [
-  {
-    id: 1,
-    title: 'New place 1',
-    description: 'SUper description 1',
-    img: '',
-    lngLat: [30.5245, 50.4514]
-  },
-  {
-    id: 2,
-    title: 'New place 2',
-    description: 'SUper description 2',
-    img: '',
-    lngLat: [30.5245, 50.4504]
-  }
-]
-
-const activeId = ref(null)
-const map = ref(null)
-
-const changeActiveId = (id) => {
-  activeId.value = id
-}
-
-const changePlace = (id) => {
-  const { lngLat } = favoritePlaces.find((place) => place.id === id)
-  changeActiveId(id)
-  map.value.flyTo({ center: lngLat })
-}
+import HomePage from './views/HomePage.vue'
 </script>
 
 <template>
-  <main class="flex h-screen">
-    <div class="bg-white h-full w-[400px] shrink-0 overflow-auto pb-10">
-      <FavoritePlaces :items="favoritePlaces" :active-id="activeId" @place-clicked="changePlace" />
-    </div>
-
-    <div class="w-full h-full flex items-center justify-center text-6xl">
-      <MapboxMap
-        class="w-full h-full"
-        :access-token="mapSettings.apiToken"
-        :map-style="mapSettings.style"
-        :center="[30.5245, 50.4504]"
-        :zoom="10"
-        @mb-created="(mapInstance) => (map = mapInstance)"
-      >
-        <MapboxMarker v-for="place in favoritePlaces" :key="place.id" :lngLat="place.lngLat">
-          <button type="button" @click="changeActiveId(place.id)">
-            <MarkerIcon class="size-8" />
-          </button>
-        </MapboxMarker>
-      </MapboxMap>
-    </div>
-  </main>
+  <HomePage />
 </template>
